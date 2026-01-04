29bd2727020696f344abe9541484cdd617d9a892
laiba-dotcom <laiba@tuvaustria.sa>
2026-01-04 12:37:56 +0300

    rename convix.py to urls.py and fix settings path

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'convix.settings')  # change 'convix' if your settings file is different
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')  # or your settings path
 
 application = get_wsgi_application()
